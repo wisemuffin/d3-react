c2a7ffe436d4eb92ad99e22c21f02d5a8d409856
wisemuffin <davidgg777@hotmail.com>
2020-04-04 11:07:57 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import * as d3 from "d3";
+import { getTimelineData, getScatterData } from "./utils/dummyData";
+
+import Timeline from "./Components/Chart/Timeline";
+import ScatterPlot from "./Components/Chart/ScatterPlot";
+import Histogram from "./Components/Chart/Histogram";
+
+import { useInterval } from "./Hooks/useInterval";
+
+import "./styles.css";
+
+const parseDate = d3.timeParse("%m/%d/%Y");
+const dateAccessor = (d) => parseDate(d.date);
+const temperatureAccessor = (d) => d.temperature;
+const humidityAccessor = (d) => d.humidity;
+
+const getData = () => ({
+  timeline: getTimelineData(),
+  scatter: getScatterData(),
+});
+const App = () => {
+  const [data, setData] = useState(getData());
+
+  useInterval(() => {
+    setData(getData());
+  }, 4000);
+
+  return (
+    <div className="App">
+      <h1>Weather Dashboard</h1>
+      <div className="App__charts">
+        <Timeline
+          data={data.timeline}
+          xAccessor={dateAccessor}
+          yAccessor={temperatureAccessor}
+          label="Temperature"
+        />
+        <ScatterPlot
+          data={data.scatter}
+          xAccessor={humidityAccessor}
+          yAccessor={temperatureAccessor}
+          xLabel="Humidity"
+          yLabel="Temperature"
+        />
+        <Histogram
+          data={data.scatter}
+          xAccessor={humidityAccessor}
+          label="Humidity"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
